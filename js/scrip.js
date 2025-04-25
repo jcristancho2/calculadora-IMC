2f10f6426c637e3b96e2899277b6becb15be799f
jcristancho2 <jocristanchool@gmail.com>
2025-04-25 10:45:52 -0500

    🚀 asignacion requerimiento

---
+++ js/scrip.js
@@ -0,0 +1,10 @@
+/*
+calculadora imc v3
+1. total hombres y mujeres registrados
+2. promedio de edad hombres y mujeres
+3. total menores registrados
+4. total hombres y mujeres con sobrepeso
+5. paciente de mas bajo imc
+*/
+
+
